5f1ba6bcbbd2e87b5a00948a13fe7337a0f28fb6
Wisnu A. Kurniawan <wisnuadi415@gmail.com>
2022-12-09 18:44:53 +0800

    Update #2
    Another adjustments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,17 @@
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 4000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
+function setAnimation() {
+    $('.count.pending').each(function() {
+        var isElementInView = Utils.isElementInView($(this), false);    
+        if (isElementInView) {
+            $(this).removeClass('pending');
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 4000,
+                easing: 'swing',
+                step: function(now) {
+                $(this).text(Math.ceil(now));
+                }
+            });
         }
     });
-});
+}
